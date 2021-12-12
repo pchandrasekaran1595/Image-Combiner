f6dbdbf5bf99d99dbd664fc13da3d511e58863de
Prashanth <67106624+pchandrasekaran1595@users.noreply.github.com>
2021-12-12 16:07:41 +0530

    Complete

--- CLI/app.py
+++ CLI/app.py
@@ -1,5 +1,46 @@
 import sys
+from os import listdir
 
+from .utils import READ_PATH, handler
 
 def run():
-    pass
+    args_1 = ["--file1", "-f1"]
+    args_2 = ["--file2", "-f2"]
+    args_3 = ["--vertical", "-v"]
+    args_4 = ["--adapt-big", "-ab"]
+    args_5 = ["--save", "-s"]
+    args_6 = "--wf"
+
+    filename_1, filename_2 = None, None
+    vertical = False
+    adapt_small = True
+    save = False
+    workflow = False
+
+    if args_1[0] in sys.argv: filename_1 = sys.argv[sys.argv.index(args_1[0]) + 1]
+    if args_1[1] in sys.argv: filename_1 = sys.argv[sys.argv.index(args_1[1]) + 1]
+
+    if args_2[0] in sys.argv: filename_2 = sys.argv[sys.argv.index(args_2[0]) + 1]
+    if args_2[1] in sys.argv: filename_2 = sys.argv[sys.argv.index(args_2[1]) + 1]
+
+    if args_3[0] in sys.argv or args_3[1] in sys.argv: vertical = True
+    if args_4[0] in sys.argv or args_4[1] in sys.argv: adapt_small = False
+    if args_5[0] in sys.argv or args_5[1] in sys.argv:  save = True
+    if args_6 in sys.argv: workflow = True
+
+    assert filename_1 is not None, "Enter argument for --file1 | -f1"
+    assert filename_2 is not None, "Enter argument for --file2 | -f2"
+
+    assert filename_1 in listdir(READ_PATH), "File 1 Not Found"
+    assert filename_2 in listdir(READ_PATH), "File 2 Not Found"
+
+    image_1 = handler.read_image(READ_PATH + "/" + filename_1)
+    image_2 = handler.read_image(READ_PATH + "/" + filename_2)
+    
+    image = handler.combine(image_1, image_2, vertical=vertical, adapt_small=adapt_small)
+
+    if not save:
+        if not workflow:
+            handler.show(image)
+    else:
+        handler.save_image(image)
--- CLI/utils.py
+++ CLI/utils.py
@@ -20,7 +20,7 @@ class Handler(object):
     def read_image(self, path: str) -> np.ndarray:
         return cv2.imread(path, cv2.IMREAD_COLOR)
 
-    def save_image(self, image) -> None:
+    def save_image(self, image: np.ndarray) -> None:
         cv2.imwrite(os.path.join(SAVE_PATH, "Processed.jpg"), image)
 
     def show(self, image: np.ndarray, title=None):
@@ -33,8 +33,39 @@ class Handler(object):
         figmanager.window.state("zoomed")
         plt.show()
     
-    def combine(self, image_1: np.ndarray, image_2: np.ndarray, orientation: str):
-        pass
+    def combine(self, image_1: np.ndarray, image_2: np.ndarray, vertical: bool, adapt_small: bool):
+        h1, w1, _ = image_1.shape
+        h2, w2, _ = image_2.shape
+
+        if vertical:
+            if w1 > w2:
+                if adapt_small:
+                    image_2 = cv2.resize(src=image_2, dsize=(w1, h2), interpolation=cv2.INTER_AREA)
+                else:
+                    image_1 = cv2.resize(src=image_1, dsize=(w2, h1), interpolation=cv2.INTER_AREA)
+
+            elif w2 > w1:
+                if adapt_small:
+                    image_1 = cv2.resize(src=image_1, dsize=(w2, h1), interpolation=cv2.INTER_AREA)
+                else:
+                    image_2 = cv2.resize(src=image_2, dsize=(w1, h2), interpolation=cv2.INTER_AREA)
+                    
+            return np.vstack((image_1, image_2))
+        
+        else:
+            if h1 > h2:
+                if adapt_small:
+                    image_2 = cv2.resize(src=image_2, dsize=(w2, h1), interpolation=cv2.INTER_AREA)
+                else:
+                    image_1 = cv2.resize(src=image_1, dsize=(w1, h2), interpolation=cv2.INTER_AREA)
+
+            elif h2 > h1:
+                if adapt_small:
+                    image_1 = cv2.resize(src=image_1, dsize=(w1, h2), interpolation=cv2.INTER_AREA)
+                else:
+                    image_2 = cv2.resize(src=image_2, dsize=(w2, h1), interpolation=cv2.INTER_AREA)
+                    
+            return np.hstack((image_1, image_2))
 
 
 handler = Handler()
