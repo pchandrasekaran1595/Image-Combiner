858d4361f55485f092c7e75c816961bc639ca381
Prashanth <67106624+pchandrasekaran1595@users.noreply.github.com>
2021-12-13 17:23:58 +0530

    Update

--- make-env.bat
+++ make-env.bat
@@ -1,4 +1,4 @@
 python -m virtualenv venv
 
-start /MAX cmd /k "title CMD && cls && cd venv/Scripts && activate && cd .. & cd .. && echo #################### && echo Installing Dependencies && echo #################### && pip install -r requirements.txt"
+start /MAX cmd /k "title CMD && cls && cd venv/Scripts && activate && cd .. && cd .. && echo #################### && echo Installing Dependencies && echo #################### && pip install -r requirements.txt"
 
